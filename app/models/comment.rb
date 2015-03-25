3730e0c8512ba4427502579b2bbcfc2a86d3ff9f
Isiah Singh <isiahsingh18@gmail.com>
2015-03-25 23:12:48 +0000

    Changing ratings to drop down

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,17 @@ class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :place
 
+	RATINGS = {
+		'One Star' => '1_star',
+		'Two Star' => '2_star',
+		'Three Star' => '3_star',
+		'Four Star' => '4_star',
+		'Five Star' => '5_star'
+	}
+
+	def humanized_rating
+		RATINGS.invert[self.rating]
+	end
+
+
 end
