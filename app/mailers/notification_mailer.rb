130b380d0c4113a945fed478f8f21a3e9ed1f20a
Isiah Singh <isiahsingh18@gmail.com>
2015-03-27 22:30:24 +0000

    Adjusting the automated email mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
- 		mail(to: "isiahsingh18@gmail.com", subject: "You just added a comment")
+  def comment_added(comment)
+  	@place = comment.place
+  	@place_creator = @place.user
+ 		mail(to: @place_creator.email, subject: "A comment has been added to #{@place.name}")
   
 	end 
 
